998079c9ce5e9c88d9ecea6df65cf9c4db2fb8ab
Jorge Cabrera <jocab17@gmail.com>
2022-12-10 19:17:51 -0300

    Registro mejorado, editor de perfiles, cambio de contraseña

--- BlogMundial/BlogMundial/settings.py
+++ BlogMundial/BlogMundial/settings.py
@@ -106,7 +106,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'es'
 
 TIME_ZONE = 'UTC'
 
@@ -126,4 +126,4 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 LOGIN_REDIRECT_URL = 'home'
-LOGOUT_REDIRECT_URL = 'home'
+LOGOUT_REDIRECT_URL = 'login'
--- BlogMundial/Members/urls.py
+++ BlogMundial/Members/urls.py
@@ -1,5 +1,13 @@
+from . import views
+#from django.contrib.auth import views as auth_views
 from django.urls import path
 from .views import *
+
+
 urlpatterns = [
     path('register/', UserRegistrationView.as_view(), name='register'),
+    path('edit_profile/', UserEditView.as_view(), name='edit_profile'),
+    #path('password/', auth_views.PasswordChangeView.as_view(template_name='registration/change_password.html')),
+    path('password/', PasswordsChangeView.as_view(template_name='registration/change_password.html')),
+    path('password_success/', views.password_success, name='password_success'),
 ]
